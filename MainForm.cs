c44472b5bdc0549d91616aa08682cb55e92edffa
ultv <ultv@inbox.ru>
2018-09-15 21:38:30 +0400

    create replaceWord

--- MainForm.cs
+++ MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace ExportToWord
 {
@@ -26,6 +27,32 @@ namespace ExportToWord
             var signDate = SignDateTimePicker.Value.ToShortDateString();
 
             // TODO: Word Export
+
+            var wordApp = new Word.Application();
+            wordApp.Visible = false;
+
+            try
+            {
+                var wordDocument = wordApp.Documents.Open(TemplateFileName);
+                ReplaceWordStub("{name}", name, wordDocument);
+                ReplaceWordStub("{enddate}", endDate, wordDocument);
+                ReplaceWordStub("{signDate}", signDate, wordDocument);
+
+                wordDocument.SaveAs2(@"c:\result.doc");
+                wordApp.Visible = true;
+            }
+            catch
+            {
+                MessageBox.Show("Произошла ошибка");
+            }
+            
+        }
+
+        private void ReplaceWordStub(string stubToReplace, string text, Word.Document wordDocument)
+        {
+            var range = wordDocument.Content;
+            range.Find.ClearFormatting();
+            range.Find.Execute(FindText: stubToReplace, ReplaceWith: text);
         }
     }
 }
