0ea9c35b3e14a8fc6f5d3683100b56ded33104c3
ultv <ultv@inbox.ru>
2018-09-15 20:52:05 +0400

    create form

---
+++ MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ExportToWord
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.EndDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.SignDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.ExportButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ФИО";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Действителен до:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Дата подписания:";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(151, 24);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(228, 20);
+            this.NameTextBox.TabIndex = 3;
+            // 
+            // EndDateTimePicker
+            // 
+            this.EndDateTimePicker.Location = new System.Drawing.Point(151, 58);
+            this.EndDateTimePicker.Name = "EndDateTimePicker";
+            this.EndDateTimePicker.Size = new System.Drawing.Size(228, 20);
+            this.EndDateTimePicker.TabIndex = 4;
+            // 
+            // SignDateTimePicker
+            // 
+            this.SignDateTimePicker.Location = new System.Drawing.Point(151, 92);
+            this.SignDateTimePicker.Name = "SignDateTimePicker";
+            this.SignDateTimePicker.Size = new System.Drawing.Size(228, 20);
+            this.SignDateTimePicker.TabIndex = 5;
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Location = new System.Drawing.Point(260, 133);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(119, 23);
+            this.ExportButton.TabIndex = 6;
+            this.ExportButton.Text = "Сформировать";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            this.ExportButton.Click += new System.EventHandler(this.ExportButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 168);
+            this.Controls.Add(this.ExportButton);
+            this.Controls.Add(this.SignDateTimePicker);
+            this.Controls.Add(this.EndDateTimePicker);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Заполнение доверенности";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.DateTimePicker EndDateTimePicker;
+        private System.Windows.Forms.DateTimePicker SignDateTimePicker;
+        private System.Windows.Forms.Button ExportButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExportToWord
+{
+    public partial class MainForm : Form
+    {
+        private readonly string TemplateFileName = @"c:\test.doc";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExportButton_Click(object sender, EventArgs e)
+        {
+            var name = NameTextBox.Text;
+            var endDate = EndDateTimePicker.Value.ToShortDateString();
+            var signDate = SignDateTimePicker.Value.ToShortDateString();
+
+            // TODO: Word Export
+        }
+    }
+}
